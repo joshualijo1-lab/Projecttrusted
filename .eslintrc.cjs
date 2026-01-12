029677fb984f2c12bc5df6f492a0538b6b71ae8a
joshualijo1-lab <joshualijo1@gmail.com>
2026-01-12 18:54:36 +0000

    Replace repository contents with index.html

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['next/core-web-vitals', 'prettier'],
-  rules: {
-    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }]
-  }
-};
